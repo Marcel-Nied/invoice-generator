5e7c175f19b0b1dc1d4b2a0bc153b37d497de17f
Marcel-Nied <89782806+Marcel-Nied@users.noreply.github.com>
2022-10-18 08:26:28 +0200

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewInvoiceComponent } from './new-invoice/new-invoice.component';
+import { PreviewInvoiceComponent } from './preview-invoice/preview-invoice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewInvoiceComponent,
+    PreviewInvoiceComponent
   ],
   imports: [
     BrowserModule,
